82a987e078ae6a087349797868dee026b5af74fc
khadetou <khadetou96@gmail.com>
2022-03-31 10:30:56 +0000

    roles guards

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Role } from './auth/role.enum';
+import { Roles } from './auth/roles.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Roles(Role.Admin)
   getHello(): string {
     return this.appService.getHello();
   }
